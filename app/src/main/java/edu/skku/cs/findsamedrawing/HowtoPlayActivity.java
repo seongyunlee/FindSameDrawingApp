542d69ec4c5bc068a3d6f22c979c1b5e558266c1
seongyunlee <priavate.seongyun@gmail.com>
2022-05-11 11:15:51 +0900

    activities setting

---
+++ app/src/main/java/edu/skku/cs/findsamedrawing/HowtoPlayActivity.java
@@ -0,0 +1,14 @@
+package edu.skku.cs.findsamedrawing;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HowtoPlayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_howto_play);
+    }
+}
