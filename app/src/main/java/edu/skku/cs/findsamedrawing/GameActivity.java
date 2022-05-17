a0674e55941b0e2e2772d2e214870d0180de9fb5
seongyunlee <priavate.seongyun@gmail.com>
2022-05-17 16:51:40 +0900

    setting Chronometer

--- app/src/main/java/edu/skku/cs/findsamedrawing/GameActivity.java
+++ app/src/main/java/edu/skku/cs/findsamedrawing/GameActivity.java
@@ -2,10 +2,12 @@ package edu.skku.cs.findsamedrawing;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Chronometer;
 
 import edu.skku.cs.findsamedrawing.databinding.ActivityGameBinding;
 
@@ -20,10 +22,25 @@ public class GameActivity extends AppCompatActivity {
         binding.executePendingBindings();
         viewModel = new ViewModelProvider(this).get(GameViewModel.class);
         setCardBoard();
+        setChronometer();
     }
     private void setCardBoard(){
         CardBoardAdapter adapter = new CardBoardAdapter(viewModel);
         binding.gvCardBoard.setAdapter(adapter);
+    }
+    private void setChronometer(){
+        binding.cmClock.setFormat("Time: %s");
+        viewModel.stop.observe(this, new Observer<Boolean>() {
+            @Override
+            public void onChanged(Boolean aBoolean) {
+                if(aBoolean==true){
+                    binding.cmClock.stop();
+                }
+                else{
+                    binding.cmClock.start();
+                }
+            }
+        });
     }
 
 }
