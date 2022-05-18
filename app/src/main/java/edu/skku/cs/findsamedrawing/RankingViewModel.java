f0b7cf90929f62963e4a44f87a8c1e3af03d93dc
seongyunlee <priavate.seongyun@gmail.com>
2022-05-19 01:17:48 +0900

    fix error

--- app/src/main/java/edu/skku/cs/findsamedrawing/RankingViewModel.java
+++ app/src/main/java/edu/skku/cs/findsamedrawing/RankingViewModel.java
@@ -11,10 +11,12 @@ import java.util.ArrayList;
 import edu.skku.cs.findsamedrawing.Model.Record;
 
 public class RankingViewModel extends ViewModel {
+
     MutableLiveData<ArrayList<Record>> _records = new MutableLiveData<>();
     LiveData<ArrayList<Record>> records = _records;
     ConnectionUtil connectionUtil = new ConnectionUtil();
-    RankingViewModel(){
+
+    public RankingViewModel() {
         super();
     }
 }
